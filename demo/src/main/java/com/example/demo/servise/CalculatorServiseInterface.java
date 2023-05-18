90d66036cebe9442dda5000cff0930d16e9caf5a
Admin <samedi450511@gmail.com>
2023-05-18 18:16:31 +0600

    hw

---
+++ demo/src/main/java/com/example/demo/servise/CalculatorServiseInterface.java
@@ -0,0 +1,13 @@
+package com.example.demo.servise;
+
+public interface CalculatorServiseInterface {
+    String welcome();
+
+    String resultPlus(String num1, String num2);
+
+    String resultMinus(String num1, String num2);
+
+    String resultMultiply(String num1, String num2);
+
+    String resultDivide(String num1, String num2);
+}
